b2e550f86b23681cd2bd3b2eef8f7a80d1d45966
Tóth Gábor <tothgabor2003@gmail.com>
2025-04-27 20:34:18 +0200

    init

---
+++ src/docker.ts
@@ -0,0 +1,208 @@
+import Docker, {
+  ContainerInspectInfo,
+  ImageInspectInfo,
+  VolumeInspectInfo,
+  NetworkInspectInfo,
+} from "dockerode";
+
+//var docker = new Docker({socketPath: '/var/run/docker-cli.sock'});
+var docker = new Docker({ socketPath: "/var/run/docker.sock" });
+
+export async function listContainers(): Promise<ContainerInspectInfo[]> {
+  try {
+    const containers = await docker.listContainers({ all: true });
+    //console.log("Containers:", containers);
+
+    // Use Promise.all to wait for all inspect calls to complete
+    const containerList = await Promise.all(
+      containers.map(async (container) => {
+        const cont = docker.getContainer(container.Id);
+        try {
+          const data = await cont.inspect(); 
+          return data;
+        } catch (err) {
+          console.error("Error inspecting container:", err);
+          throw err;
+        }
+      })
+    );
+
+    return containerList; // Return the fully resolved list
+  } catch (err) {
+    console.error("Error listing containers:", err);
+    throw err;
+  }
+}
+
+export async function stopContainer(id: string): Promise<void> {
+    try {
+        const container = docker.getContainer(id);
+        await container.stop();
+        console.log(`Container ${id} stopped successfully`);
+    }
+    catch (err) {
+        console.error(`Error stopping container ${id}:`, err);
+        throw err;
+    }
+}
+
+export async function startContainer(id: string): Promise<void> {
+    try {
+        const container = docker.getContainer(id);
+        await container.start();
+        console.log(`Container ${id} started successfully`);
+    }
+    catch (err) {
+        console.error(`Error starting container ${id}:`, err);
+        throw err;
+    }
+}
+
+export async function deleteContainer(id: string): Promise<boolean> {
+    try {
+        const container = docker.getContainer(id);
+        await container.stop();
+        await container.remove();
+        console.log(`Container ${id} deleted successfully`);
+        return true;
+    }
+    catch (err) {
+        console.error(`Error deleting container ${id}:`, err);
+        throw err;
+    }
+}
+
+export async function restartContainer(id: string): Promise<void> {
+    try {
+        const container = docker.getContainer(id);
+        await container.restart();
+        console.log(`Container ${id} restarted successfully`);
+    }
+    catch (err) {
+        console.error(`Error restarting container ${id}:`, err);
+        throw err;
+    }
+}
+
+export async function pauseContainer(id: string): Promise<void> {
+    try {
+        const container = docker.getContainer(id);
+        await container.pause();
+        console.log(`Container ${id} paused successfully`);
+    }
+    catch (err) {
+        console.error(`Error pausing container ${id}:`, err);
+        throw err;
+    }
+}
+export async function unpauseContainer(id: string): Promise<void> {
+    try {
+        const container = docker.getContainer(id);
+        await container.unpause();
+        console.log(`Container ${id} unpaused successfully`);
+    }
+    catch (err) {
+        console.error(`Error unpausing container ${id}:`, err);
+        throw err;
+    }
+}
+
+export async function createContainer(
+  image: string,
+  name: string,
+  options: { [key: string]: any }
+): Promise<ContainerInspectInfo> {
+  try {
+    const container = await docker.createContainer({
+      Image: image,
+      name: name,
+      ...options,
+    });
+    await container.start();
+    console.log(`Container ${name} created and started successfully`);
+    const data = await container.inspect();
+    console.log("Container data:", data);
+    return data;
+  } catch (err) {
+    console.error(`Error creating container ${name}:`, err);
+    throw err;
+  }
+}
+
+
+export async function listImages(): Promise<ImageInspectInfo[]> {
+  try {
+    const images = await docker.listImages();
+    //console.log("Images:", images);
+    const imageList = await Promise.all(
+      images.map(async (image) => {
+        const cont = docker.getImage(image.Id);
+        try {
+          const data = await cont.inspect();
+          //console.log("Container data:", data);
+          return data;
+        } catch (err) {
+          console.error("Error inspecting container:", err);
+          throw err; 
+        }
+      })
+    );
+
+    return imageList;
+  } catch (err) {
+    console.error("Error listing images:", err);
+    throw err;
+  }
+}
+
+export async function listNetworks(): Promise<NetworkInspectInfo[]> {
+  try {
+    const networks = await docker.listNetworks();
+    //console.log("Netwokrs:", networks);
+    const networksList = await Promise.all(
+      networks.map(async (network) => {
+        const netw = docker.getNetwork(network.Id);
+        try {
+          const data = await netw.inspect(); 
+          //console.log("Container data:", data);
+          return data; 
+        } catch (err) {
+          console.error("Error inspecting container:", err);
+          throw err;
+        }
+      })
+    );
+
+    return networksList;
+  } catch (err) {
+    console.error("Error listing images:", err);
+    throw err;
+  }
+}
+
+export async function listVolumes(): Promise<VolumeInspectInfo[]> {
+  try {
+    const { Volumes, Warnings } = await docker.listVolumes(); 
+    if (Warnings) {
+      console.warn("Warnings:", Warnings);
+    }
+
+    const volumeList = await Promise.all(
+      Volumes.map(async (volume) => {
+        const vol = docker.getVolume(volume.Name);
+        try {
+          const data = await vol.inspect();
+          return data; 
+        } catch (err) {
+          console.error("Error inspecting volume:", err);
+          throw err;
+        }
+      })
+    );
+
+    return volumeList;
+  } catch (err) {
+    console.error("Error listing volumes:", err);
+    throw err;
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,214 @@
+// src/index.ts
+import express from "express";
+import { Request, Response } from "express";
+import cors from "cors";
+import {
+  listContainers,
+  listImages,
+  listNetworks,
+  listVolumes,
+  createContainer,
+  deleteContainer,
+  restartContainer,
+  stopContainer,
+  startContainer,
+  pauseContainer,
+  unpauseContainer,
+} from "./docker";
+
+const app = express();
+const port = 3300;
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("This is an API for Docker webUI");
+});
+
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({ status: "OK" });
+});
+
+// List all containers
+app.get("/containers", async (req: Request, res: Response) => {
+  try {
+    const containers = await listContainers();
+    res.json(containers); // Send the result as a JSON response
+  } catch (err) {
+    console.error("Error listing containers:", err);
+    res.status(500).json({ error: "Failed to list containers" });
+  }
+});
+
+// Create a container
+app.post("/containers", async (req: Request, res: Response): Promise<void> => {
+  const name = req.body.containerName;
+  const image = req.body.imageName;
+  const ports = req.body.ports || [];
+  const command = req.body.command;
+  const autoremove = req.body.autoremove || false;
+
+  console.log("Received request to create container:", {
+    name,
+    image,
+    ports,
+    command,
+    autoremove,
+  });
+
+  if (!image || !name) {
+    res.status(400).json({ error: "Image and name are required" });
+    return;
+  }
+
+  try {
+    const container = await createContainer(image, name, {
+      ExposedPorts: ports,
+      Cmd: command,
+      AutoRemove: autoremove,
+    });
+    res.status(201).json(container);
+  } catch (err) {
+    console.error("Error creating container:", err);
+    res.status(500).json({ error: "Failed to create container" });
+  }
+});
+
+
+app.get("/containers/:id", async (req: Request, res: Response) => {
+  res.send("This is not implemented yet");
+});
+
+// Delete a container
+app.delete("/containers", async (req: Request, res: Response) => {
+  const containerId = req.body.containerId;
+  if (!containerId) {
+    res.status(400).json({ error: "Container ID is required" });
+    return;
+  }
+
+  try {
+    const container = await deleteContainer(containerId);
+    if (!container) {
+      res.status(404).json({ error: "Container not found" });
+      return;
+    }
+    res.status(200).json({ message: "Container deleted successfully" });
+  } catch (err) {
+    console.error("Error deleting container:", err);
+    res.status(500).json({ error: "Failed to delete container" });
+  }
+});
+
+// Restart a container
+app.post("/containers/restart", async (req: Request, res: Response) => {
+  const containerId = req.body.containerId;
+  if (!containerId) {
+    res.status(400).json({ error: "Container ID is required" });
+    return;
+  }
+
+  try {
+    await restartContainer(containerId);
+    res.status(200).json({ message: "Container restarted successfully" });
+  } catch (err) {
+    console.error("Error restarting container:", err);
+    res.status(500).json({ error: "Failed to restart container" });
+  }
+});
+
+// Stop a container
+app.post("/containers/stop", async (req: Request, res: Response) => {
+  const containerId = req.body.containerId;
+  if (!containerId) {
+    res.status(400).json({ error: "Container ID is required" });
+    return;
+  }
+
+  try {
+    await stopContainer(containerId);
+    res.status(200).json({ message: "Container stopped successfully" });
+  } catch (err) {
+    console.error("Error stopping container:", err);
+    res.status(500).json({ error: "Failed to stop container" });
+  }
+});
+// Start a container
+app.post("/containers/start", async (req: Request, res: Response) => {
+  const containerId = req.body.containerId;
+  if (!containerId) {
+    res.status(400).json({ error: "Container ID is required" });
+    return;
+  }
+  try {
+    await startContainer(containerId);
+    res.status(200).json({ message: "Container started successfully" });
+  } catch (err) {
+    console.error("Error starting container:", err);
+    res.status(500).json({ error: "Failed to start container" });
+  }
+});
+// Pause a container
+app.post("/containers/pause", async (req: Request, res: Response) => {
+  const containerId = req.body.containerId;
+  if (!containerId) {
+    res.status(400).json({ error: "Container ID is required" });
+    return;
+  }
+  try {
+    await pauseContainer(containerId);
+    res.status(200).json({ message: "Container paused successfully" });
+  } catch (err) {
+    console.error("Error pausing container:", err);
+    res.status(500).json({ error: "Failed to pause container" });
+  }
+});
+// Unpause a container
+app.post("/containers/unpause", async (req: Request, res: Response) => {
+  const containerId = req.body.containerId;
+  if (!containerId) {
+    res.status(400).json({ error: "Container ID is required" });
+    return;
+  }
+  try {
+    await unpauseContainer(containerId);
+    res.status(200).json({ message: "Container unpaused successfully" });
+  } catch (err) {
+    console.error("Error unpausing container:", err);
+    res.status(500).json({ error: "Failed to unpause container" });
+  }
+});
+
+// List all images
+app.get("/images", async (req: Request, res: Response) => {
+  try {
+    const images = await listImages();
+    res.json(images);
+  } catch (err) {
+    console.error("Error listing images:", err);
+    res.status(500).json({ error: "Failed to list images" }); 
+  }
+});
+
+app.get("/networks", async (req: Request, res: Response) => {
+  try {
+    const networks = await listNetworks();
+    res.json(networks);
+  } catch (err) {
+    console.error("Error listing networks:", err);
+    res.status(500).json({ error: "Failed to list networks" });
+  }
+});
+app.get("/volumes", async (req: Request, res: Response) => {
+  try {
+    const volumes = await listVolumes();
+    res.json(volumes);
+  } catch (err) {
+    console.error("Error listing volumes:", err);
+    res.status(500).json({ error: "Failed to list volumes" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
